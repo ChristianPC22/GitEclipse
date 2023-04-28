6d90b6d8a2cafac5ac1f5613a89580486adcd309
Christian Peraza Cuenca <christianperaza22@gmail.com>
2023-04-28 12:05:49 +0200

    Creado un toString para mostrar los objetos

--- Entornos_GitEclipse/src/Ejercicio1/Alumno.java
+++ Entornos_GitEclipse/src/Ejercicio1/Alumno.java
@@ -8,4 +8,10 @@ public class Alumno
 	{
 		
 	}
+	
+	@Override
+	public String toString() 
+	{
+		return "Alumno: [Nombre: " + nombre + "], " + "[Apellido: " + apellido + "], " + "[DNI: " + dni + "]";
+	}
 }
