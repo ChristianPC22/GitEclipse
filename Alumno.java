334fab783b7c116003cfb340998461134e11184a
Christian <127390573+ChristianPC22@users.noreply.github.com>
2023-04-29 12:19:01 +0200

    Add files via upload

---
+++ Alumno.java
@@ -0,0 +1,18 @@
+package Ejercicio1;
+
+public class Alumno
+{
+	private String nombre, apellido, dni;
+	
+	public Alumno(String nombre, String apellido, String dni) 
+	{
+		
+	}
+	
+	@Override
+	public String toString() 
+	{
+		return "Alumno: [Nombre: " + nombre + "], " + "[Apellido: " + apellido + "], " + "[DNI: " + dni + "]";
+	}
+
+}
---
+++ Principal.java
@@ -0,0 +1,15 @@
+package Tarea1;
+
+public class Principal 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Listado de alumnos 1º DAW");
+		System.out.println("Alumno 1:");
+
+		Alumno alumno1 = new Alumno("Christian", "Peraza", "50124756N");
+
+		System.out.println(alumno1);
+	}
+
+}
